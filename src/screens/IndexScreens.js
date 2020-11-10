5e593595f119bfd0e8c6699c07dca1898073d05e
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-11-09 18:08:15 -0600

    created edit feature

--- src/screens/IndexScreens.js
+++ src/screens/IndexScreens.js
@@ -1,49 +1,73 @@
-import React, { useContext } from 'react'
-import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-native'
-import {Context} from '../context/BlogContext'
-import {Feather} from '@expo/vector-icons' 
+import React, { useContext } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    FlatList,
+    Button,
+    TouchableOpacity,
+} from "react-native";
+import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
-const IndexScreen = ({navigation}) => {
-    const {state, addBlogPost, deleteBlogPost} = useContext(Context)
+const IndexScreen = ({ navigation }) => {
+    const { state, deleteBlogPost } = useContext(Context);
 
     return (
         <View>
-            <Button title='Add Post' onPress={addBlogPost} />
-            <FlatList 
-                data={state} 
+            <FlatList
+                data={state}
                 keyExtractor={(blogPost) => blogPost.id.toString()}
-                renderItem= {({ item }) => {
+                renderItem={({ item }) => {
                     return (
-                        <TouchableOpacity onPress={() => navigation.navigate('Show', {id: item.id})}>
+                        <TouchableOpacity
+                            onPress={() =>
+                                navigation.navigate("Show", { id: item.id })
+                            }
+                        >
                             <View style={styles.row}>
-                                <Text style={styles.title}>{item.title} -{item.id}</Text>
-                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <Text style={styles.title}>
+                                    {item.title} -{item.id}
+                                </Text>
+                                <TouchableOpacity
+                                    onPress={() => deleteBlogPost(item.id)}
+                                >
                                     <Feather style={styles.icon} name='trash' />
                                 </TouchableOpacity>
                             </View>
                         </TouchableOpacity>
-                    )
+                    );
                 }}
-                    />
+            />
         </View>
-    )
-}
+    );
+};
+
+IndexScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+                <Feather name='plus' size={30} />
+            </TouchableOpacity>
+        ),
+    };
+};
 
 const styles = StyleSheet.create({
     row: {
-        flexDirection:'row',
+        flexDirection: "row",
         justifyContent: "space-between",
         paddingVertical: 20,
         paddingHorizontal: 10,
         borderTopWidth: 1,
-        borderColor: 'gray'
+        borderColor: "gray",
     },
     title: {
-        fontSize: 18
+        fontSize: 18,
     },
     icon: {
-        fontSize: 24
-    }
-})
+        fontSize: 24,
+    },
+});
 
-export default IndexScreen
+export default IndexScreen;
--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,22 +1,39 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { Context } from "../context/BlogContext";
+import { EvilIcons } from "@expo/vector-icons";
 
-import React, {useContext} from 'react'
-import {View, Text, StyleSheet} from 'react-native'
-import {Context} from '../context/BlogContext'
+const ShowScreen = ({ navigation }) => {
+    const { state } = useContext(Context);
 
-const ShowScreen = ({navigation}) => {
-    const {state} = useContext(Context)
+    const blogPost = state.find(
+        (blogPost) => blogPost.id === navigation.getParam("id"),
+    );
 
-    const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
-
-    return(
+    return (
         <View>
             <Text>{blogPost.title}</Text>
+            <Text>{blogPost.content}</Text>
         </View>
-    )
-}
+    );
+};
 
-const styles = StyleSheet.create({
+ShowScreen.navigationOptions = ({ navigation }) => {
+    return {
+        headerRight: () => (
+            <TouchableOpacity
+                onPress={() =>
+                    navigation.navigate("Edit", {
+                        id: navigation.getParam("id"),
+                    })
+                }
+            >
+                <EvilIcons name='pencil' size={35} />
+            </TouchableOpacity>
+        ),
+    };
+};
 
-})
+const styles = StyleSheet.create({});
 
-export default ShowScreen
+export default ShowScreen;
